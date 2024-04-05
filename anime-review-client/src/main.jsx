f5e6cd41ce843c1e519c6a4c7c4432fb9e9489ae
Diego Engelhard <00356119@uca.edu.sv>
2024-04-04 19:40:25 -0600

    feat(client): mount router dom

--- anime-review-client/src/main.jsx
+++ anime-review-client/src/main.jsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { BrowserRouter } from 'react-router-dom';
+
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      <ToastContainer />
+    </BrowserRouter>
   </React.StrictMode>,
 )
