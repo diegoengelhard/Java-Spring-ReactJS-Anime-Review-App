2ad20d9106ed8be74599c026a1b2e9b186be46cf
Diego Engelhard <00356119@uca.edu.sv>
2024-04-08 13:34:53 -0600

    refactor(prisma): project folder structure

--- anime-review-prisma-orm/prisma/migrations/20240408193058_migration/migration.sql
+++ anime-review-prisma-orm/prisma/migrations/20240408193058_migration/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
 CREATE TABLE "reviews" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
     "title" TEXT NOT NULL,
     "link" TEXT,
     "rating" INTEGER,
